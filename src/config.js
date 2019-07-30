32880fa557fdf0db85bfa316cfd75f8449c6e9d1
Aidan Curtis <>
2019-07-30 14:23:42 -0700

    updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_PATH:"./src",
 	REACT_APP_MAP_KEY:"AIzaSyAYdp6JWyJaD-2Jiil3ggePxUBDIMpD-Kg",
-	REACT_APP_API_ENDPOINT:"http://api.cherrypick-analytics.com"
+	REACT_APP_API_ENDPOINT:"https://api.cherrypick-analytics.com"
 }
